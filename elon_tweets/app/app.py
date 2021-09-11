89997cf61604983a34822f55530a9b2ca78f0545
Jan Ciechowski <jan@multiply.ai>
2021-09-11 13:21:47 +0100

    fix: getting app to run nicely

--- elon_tweets/app/app.py
+++ elon_tweets/app/app.py
@@ -1,3 +1,4 @@
+import logging
 import google.auth
 from flask import Flask
 from google.cloud import secretmanager_v1 as secretmanager
@@ -9,32 +10,37 @@ def get_secret_value(client, project, secret, version='latest'):
     return response.payload.data.decode('UTF-8')
 
 
-def load_secrets(secrets):
-	client = secretmanager.SecretManagerServiceClient()
-
+def load_secrets(project_id, secrets):
+    client = secretmanager.SecretManagerServiceClient()
     secret_vars = {}
+
     for secret in secrets:
         secret_vars[secret] = get_secret_value(client, project_id, secret)
 
     return secret_vars
-ą
+
 
 
 def create_app():
-	app = Flask(__name__)
-	_, project_id = google.auth.default()
+    app = Flask(__name__)
+    _ ,project_id = google.auth.default()
+
+    logging.info(f'Running app for project {project_id}.')
 
 
-	secret_keys = [
-	"TWITTER_API_BEARER_TOKEN",
-	"TWITTER_API_KEY",
-	"TWITTER_API_SECRET"
-	]
-
-	secrets = load_secrets(
-		project_id
+    secret_keys = [
+    "TWITTER_API_BEARER_TOKEN",
+    "TWITTER_API_KEY",
+    "TWITTER_API_SECRET"
+    ]
+    secrets = load_secrets(
+        project_id,
         secret_keys,
     )
 
-	for key, secret in secrets.items():
-		app.config[key] = secret
+    for key, secret in secrets.items():
+        app.config[key] = secret
+
+    logging.info('Secrets loaded.')
+
+    return app
