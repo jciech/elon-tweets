b95b85d37101111d964e92feab9345c801fe45e1
Jan Ciechowski <jan@multiply.ai>
2021-09-11 16:16:23 +0100

    chore: add positional argument to main.py

--- elon_tweets/main.py
+++ elon_tweets/main.py
@@ -4,7 +4,7 @@ import os, sys
 sys.path.append(os.path.dirname(__file__))
 
 
-app = create_app()
+app = create_app(None)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
