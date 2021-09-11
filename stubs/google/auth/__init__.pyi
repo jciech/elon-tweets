5d5bbbb5e0d0ed38e0fb6923eaba7f6b57930f80
Jan Ciechowski <jan@multiply.ai>
2021-09-11 16:38:43 +0100

    chore: add google cloud stubs

---
+++ stubs/google/auth/__init__.pyi
@@ -0,0 +1,1 @@
+from google.auth._default import default as default, load_credentials_from_file as load_credentials_from_file
---
+++ stubs/google/auth/compute_engine/__init__.pyi
@@ -0,0 +1,1 @@
+from google.auth.compute_engine.credentials import Credentials as Credentials, IDTokenCredentials as IDTokenCredentials
