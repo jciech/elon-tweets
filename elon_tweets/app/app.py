8d6c688fc681b5a19b29033b4568ebd133a9a3f3
Jan Ciechowski <jan@multiply.ai>
2021-09-11 12:58:03 +0100

    feat: initialise flask app, load secrets

---
+++ elon_tweets/app/app.py
@@ -0,0 +1,40 @@
+import google.auth
+from flask import Flask
+from google.cloud import secretmanager_v1 as secretmanager
+
+
+def get_secret_value(client, project, secret, version='latest'):
+    name = f'projects/{project}/secrets/{secret}/versions/{version}'
+    response = client.access_secret_version(request={'name': name})
+    return response.payload.data.decode('UTF-8')
+
+
+def load_secrets(secrets):
+	client = secretmanager.SecretManagerServiceClient()
+
+    secret_vars = {}
+    for secret in secrets:
+        secret_vars[secret] = get_secret_value(client, project_id, secret)
+
+    return secret_vars
+ą
+
+
+def create_app():
+	app = Flask(__name__)
+	_, project_id = google.auth.default()
+
+
+	secret_keys = [
+	"TWITTER_API_BEARER_TOKEN",
+	"TWITTER_API_KEY",
+	"TWITTER_API_SECRET"
+	]
+
+	secrets = load_secrets(
+		project_id
+        secret_keys,
+    )
+
+	for key, secret in secrets.items():
+		app.config[key] = secret
